c32a5a259aac4c344513821c9742eb52df03d522
JC <jcdotnet@hotmail.com>
2026-04-27 18:33:44 +0200

    fix(map): marker icon

--- src/components/events/Map/Map.tsx
+++ src/components/events/Map/Map.tsx
@@ -1,6 +1,19 @@
 
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+import L from 'leaflet';
+
+delete (L.Icon.Default.prototype as any)._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
+
 interface MapProps {
   lat: number;
   lng: number;
